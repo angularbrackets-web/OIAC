b349b8497114a49573240d0317c1cd8870d92be4
Mohammad Najimudeen <ibrahim_na@outlook.com>
2023-10-31 08:32:24 -0600

    newsletter subscription done, working on getting prayer times from hygraph

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,34 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+                'fade-in-down': {
+                    '0%': {
+                        opacity: '0',
+                        transform: 'translateY(-10px)'
+                    },
+                    '100%': {
+                        opacity: '1',
+                        transform: 'translateY(0)'
+                    },
+                },
+                'fade-out-down': {
+                    'from': {
+                        opacity: '1',
+                        transform: 'translateY(0px)'
+                    },
+                    'to': {
+                        opacity: '0',
+                        transform: 'translateY(10px)'
+                    },
+                },
+				animation: {
+					'fade-in-down': 'fade-in-down 0.5s ease-out',
+					'fade-out-down': 'fade-out-down 0.5s ease-out',
+				}
+			}
+		},
 	},
 	plugins: [],
 }
