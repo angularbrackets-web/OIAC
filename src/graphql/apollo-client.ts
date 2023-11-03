ea5d5d2776b41c214d1ef8b5d9e6c3c581bdfe33
Mohammad Najimudeen <ibrahim_na@outlook.com>
2023-11-03 12:10:31 -0600

    fix apollo client import url

--- src/graphql/apollo-client.ts
+++ src/graphql/apollo-client.ts
@@ -1,4 +1,4 @@
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client/core/core.cjs"
 
 const client = new ApolloClient({
     uri: 'https://api-us-west-2.hygraph.com/v2/clo0kf2c97pnm01t4g8sva2ni/master',
