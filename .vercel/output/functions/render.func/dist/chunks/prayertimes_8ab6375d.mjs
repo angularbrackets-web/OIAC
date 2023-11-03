1494deeef390fbadb63127702b94f07298a9164b
Mohammad Najimudeen <ibrahim_na@outlook.com>
2023-11-02 19:58:36 -0600

    build

---
+++ .vercel/output/functions/render.func/dist/chunks/prayertimes_8ab6375d.mjs
@@ -0,0 +1,8 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+import 'react';
+import 'react-dom/server';
+
+const page = () => import('./pages/prayertimes_b2a1bb49.mjs');
+
+export { page };
